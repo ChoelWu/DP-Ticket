8bcb8795689c1c2637b5c2d6e9fc5d7ce56e39a0
ChoelWu <38238531+ChoelWu@users.noreply.github.com>
2018-08-25 17:14:03 +0800

    solve bugs

--- action.php
+++ action.php
@@ -1,19 +1,19 @@
 <?php
 $action = $_REQUEST['action'];
 if ($action == 'upload') {
-	$file_name = "upload/" . time() . ".png";
-	$b = move_uploaded_file($_FILES["ticket_bg"]["tmp_name"], $file_name);
-	echo $file_name;
+    $file_name = "upload/" . time() . ".png";
+    $b = move_uploaded_file($_FILES["ticket_bg"]["tmp_name"], $file_name);
+    echo $file_name;
 }
 
 if ($action == 'save') {
-	$tpl = $_REQUEST['tpl'];
-	$html_header = '<!DOCTYPE html><html><head lang="en"><meta charset="UTF-8"><title>Tdrag</title><link href="demo.css" type="text/css" rel="stylesheet"> <script type="text/javascript" src="JavaScript/LodopFuncs.js"> </script> <style> .boxList{ border: 1px solid #ff0033; height: 387px; position: relative; width: 600px; background: url(/DP-Ticket/image/default.png) no-repeat center center;
-		margin: 0px auto;} .title { cursor: move; } .item textarea { background-color: transparent; overflow:hidden; resize:none; } textarea { margin-left: 10px; } </style></head>';
-	$foot = '</body></html>';
-	$file_name = "tpl/tpl-" . time() . ".html";
-	$file = fopen($file_name, 'w');
-	fwrite($file, $html_header . $tpl . $foot);
-	fclose($file);
-	echo $file_name;
+    $tpl = $_REQUEST['tpl'];
+    $html_header = '<!DOCTYPE html><html><head lang="en"><meta charset="UTF-8"><title>Tdrag</title><link href="demo.css" type="text/css" rel="stylesheet"> <script type="text/javascript" src="JavaScript/LodopFuncs.js"> </script> <style> .boxList{ height: 387px; position: relative; width: 600px; background: url(/DP-Ticket/image/default.png) no-repeat center center;
+		margin: 0px auto;} .title { cursor: move; } .item textarea { background-color: transparent; overflow:hidden; resize:none; } textarea { margin-left: 10px; } </style></head><body>';
+    $foot = '</body></html>';
+    $file_name = "tpl/tpl-" . time() . ".html";
+    $file = fopen($file_name, 'w');
+    fwrite($file, $html_header . $tpl . $foot);
+    fclose($file);
+    echo $file_name;
 }
--- print.php
+++ print.php
@@ -1,15 +1,18 @@
 <?php
-include('./tpl.html');
+include('./tpl/tpl-1535013263.html');
 ?>
 <a href="javascript:myPREVIEW1();">打印预览</a>
 <script>
 var LODOP; //声明为全局变量
-	function myPREVIEW1() {	
-		LODOP=getLodop(); 
+console.log(document.getElementById("boxList").innerHTML);
+	function myPREVIEW1() {
+		LODOP=getLodop();
 		LODOP.PRINT_INITA(0,0,800,706,"打印控件功能演示_Lodop功能_超文本内容缩放打印");
-		LODOP.ADD_PRINT_IMAGE(45,275,491,538,document.documentElement.innerHTML);
-		LODOP.SET_PRINT_STYLEA(0,"Stretch",2);
+		// LODOP.ADD_PRINT_IMAGE(45,275,491,538,document.documentElement.innerHTML);
+        LODOP.ADD_PRINT_IMAGE(5,"10%","80%",140, document.getElementById("boxList").innerHTML);
+        LODOP.SET_PRINT_STYLEA(0,"Stretch",2);
 		LODOP.SET_PRINT_STYLEA(0,"FontColor","#FF0000");
-		LODOP.PREVIEW();		
+        LODOP.SET_PRINT_STYLEA(0,"Horient",2);
+		LODOP.PREVIEW();
 	};
 </script>
