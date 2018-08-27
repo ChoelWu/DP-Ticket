0b368e9c708430a4fa75c767c305ba0a010613cc
ChoelWu <38238531+ChoelWu@users.noreply.github.com>
2018-08-27 11:57:46 +0800

    edit  style
    edit  style

--- action.php
+++ action.php
@@ -8,9 +8,9 @@ if ($action == 'upload') {
 
 if ($action == 'save') {
     $tpl = $_REQUEST['tpl'];
-    $html_header = '<!DOCTYPE html><html><head lang="en"><meta charset="UTF-8"><title>Tdrag</title><link href="demo.css" type="text/css" rel="stylesheet"> <script type="text/javascript" src="JavaScript/LodopFuncs.js"> </script> <style> .boxList{ height: 387px; position: relative; width: 600px; background: url(/DP-Ticket/image/default.png) no-repeat center center;
-		margin: 0px auto;} .title { cursor: move; } .item textarea { background-color: transparent; overflow:hidden; resize:none; } textarea { margin-left: 10px; } </style></head><body>';
-    $foot = '</body></html>';
+    $html_header = '<!DOCTYPE html><html><head lang="en"><meta charset="UTF-8"><title>DP-Ticket</title><link href="demo.css" type="text/css" rel="stylesheet"> <script type="text/javascript" src="JavaScript/LodopFuncs.js"> <style> .boxList{ height: 387px; position: relative; width: 600px; background: url(/DP-Ticket/image/default.png) no-repeat center center;
+		margin: 0px auto;} .item textarea { background-color: transparent; overflow:hidden; resize:none; } .item { padding-left: 10px; } </style></head><body><div id="boxList">';
+    $foot = '</div></body></html>';
     $file_name = "tpl/tpl-" . time() . ".html";
     $file = fopen($file_name, 'w');
     fwrite($file, $html_header . $tpl . $foot);
--- print.php
+++ print.php
@@ -1,18 +1,68 @@
 <?php
-include('./tpl/tpl-1535013263.html');
+if(!empty($_GET['has_tpl'])) {
+    include('./tpl/' . $_GET['has_tpl']);
+}
+
+$files = scandir('./tpl/');
+unset($files['0']);
+unset($files['1']);
+$options = '';
+foreach ($files as $file) {
+    $options .= '<option>' . $file . '</option>';
+}
 ?>
-<a href="javascript:myPREVIEW1();">打印预览</a>
+<div id="extra" style="text-align:center; margin-top: 20px;">
+    <label for="">选择模板</label>
+    <select name="" id="select-tpl">
+        <?php echo $options ?>
+    </select>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="javascript:print();">打印</a>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="javascript:selectAsDefaultPrinter();">选择默认打印机</a>
+</div>
+<script type="text/javascript" src="JavaScript/jquery1.7.2.js"></script>
 <script>
-var LODOP; //声明为全局变量
-console.log(document.getElementById("boxList").innerHTML);
-	function myPREVIEW1() {
-		LODOP=getLodop();
-		LODOP.PRINT_INITA(0,0,800,706,"打印控件功能演示_Lodop功能_超文本内容缩放打印");
-		// LODOP.ADD_PRINT_IMAGE(45,275,491,538,document.documentElement.innerHTML);
-        LODOP.ADD_PRINT_IMAGE(5,"10%","80%",140, document.getElementById("boxList").innerHTML);
-        LODOP.SET_PRINT_STYLEA(0,"Stretch",2);
-		LODOP.SET_PRINT_STYLEA(0,"FontColor","#FF0000");
-        LODOP.SET_PRINT_STYLEA(0,"Horient",2);
-		LODOP.PREVIEW();
-	};
+    var LODOP;
+    $.getJSON("/DP-Ticket/variable.json", function (data) {
+        $('#boxList .item textarea').val(function () {
+            return data[$(this).attr('name')];
+        });
+    });
+
+    $("#select-tpl").change(function() {
+        window.location.href = 'print.php?has_tpl=' + $("#select-tpl option:selected").text();
+    });
+
+    function selectAsDefaultPrinter() {
+        LODOP = getLodop();
+        if (LODOP.CVERSION) {
+            LODOP.On_Return = function (TaskID, Value) {
+                if (Value >= 0) alert("选择成功!"); else alert("选择失败！");
+            };
+            LODOP.SELECT_PRINTER();
+            return;
+        }
+        if (LODOP.SELECT_PRINTER() >= 0)
+            alert("选择成功!"); else alert("选择失败！");
+    }
+
+    function print() {
+        $("#extra").remove();
+        $('#boxList .item').css('width', function () {
+            return $(this).find('textarea').css('width');
+        });
+        $('#boxList .item').css('height', function () {
+            return $(this).find('textarea').css('height');
+        });
+        $('#boxList .item').html(function () {
+            return $(this).find('textarea').val();
+        });
+        LODOP.PRINT_INITA(0, 0, 800, 706, "打印控件功能演示_Lodop功能_超文本内容缩放打印");
+        LODOP.ADD_PRINT_IMAGE(45, 275, 491, 538, document.documentElement.innerHTML);
+        LODOP.SET_PRINT_STYLEA(0, "Stretch", 2);
+        LODOP.SET_PRINT_STYLEA(0, "FontColor", "#FF0000");
+        LODOP.SET_PRINT_STYLEA(0, "Horient", 2);
+        LODOP.PRINT();
+    }
 </script>
